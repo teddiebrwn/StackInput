f6db60a00d4df4ff5dab37b1da39e734a7562888
Tranvu <huynhdong1115@gmail.com>
2025-07-31 18:07:12 +0700

    v00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { ModeToggle } from "@/app/ui/mode-toggle";
 import InputStack from "@/app/inputStack/page";
 
 export default function Page() {
   return (
     <div>
-      <ModeToggle />
       <InputStack />
-      <h1>Welcome to My Website</h1>
-      <p>This is a sample page.</p>
     </div>
   );
 }
